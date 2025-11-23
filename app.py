64a94d6db087ed405f411ab5aa7494ab8c93d053
Tayler O'Connor <tayleroconnor@usf.edu>
2025-11-23 14:47:26 -0500

    Task3 Calculation

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ print("Welcome to my Python Program!! :D")
 
 #Task 2 - Input
 hours = input("How many hours did you sleep last night? ")
+
+#Task 3 - Calculation
+hours = float(hours)
+monthly_sleep = hours * 30
