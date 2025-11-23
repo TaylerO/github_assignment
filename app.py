334f226894b522037d8e90107a13df897143dfb3
Tayler O'Connor <tayleroconnor@usf.edu>
2025-11-23 14:49:44 -0500

    Task4 Display

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ hours = input("How many hours did you sleep last night? ")
 #Task 3 - Calculation
 hours = float(hours)
 monthly_sleep = hours * 30
+
+#Task4 - Display
+print(f"You sleep an estimated {monthly_sleep} hours of sleep per month.")
